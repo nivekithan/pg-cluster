791b42fdfe0bfb18df5289985c8fae6b4c5ec1d5
nivekithan <nivekithan363@gmail.com>
2025-08-03 17:54:47 +0530

    Working deloyment

--- controller/internal/controller/postgres_controller.go
+++ controller/internal/controller/postgres_controller.go
@@ -18,10 +18,17 @@ package controller
 
 import (
 	"context"
+	"fmt"
 
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/utils/ptr"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 
 	databasev1 "github.com/nivekithan/pg-cluster/api/v1"
@@ -36,6 +43,7 @@ type PostgresReconciler struct {
 // +kubebuilder:rbac:groups=database.kube.nivekithan.com,resources=postgres,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=database.kube.nivekithan.com,resources=postgres/status,verbs=get;update;patch
 // +kubebuilder:rbac:groups=database.kube.nivekithan.com,resources=postgres/finalizers,verbs=update
+// +kubebuilder:rbac:groups=apps,resources=deployments,verbs=get;list;watch;create;update;patch;delete
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
@@ -58,15 +66,112 @@ func (r *PostgresReconciler) Reconcile(ctx context.Context, req ctrl.Request) (c
 
 	log.Info("Found Postgres instance", "postgres", postgres)
 
-	// TODO(user): your logic here
+	postgresDeployment, err := r.ensureDeployment(ctx, postgres)
+	if err != nil {
+		log.Error(err, "Failed to ensure Deployment")
+		return ctrl.Result{}, err
+	}
+
+	log.Info("Ensured Deployment", "deploymentName", postgresDeployment.Name)
 
 	return ctrl.Result{}, nil
+}
+
+func (r *PostgresReconciler) ensureDeployment(ctx context.Context, postgres databasev1.Postgres) (*appsv1.Deployment, error) {
+	log := logf.FromContext(ctx)
+	deploymentName := fmt.Sprintf("%s-deployment", postgres.Name)
+
+	var postgresDeployment appsv1.Deployment
+
+	if err := r.Get(ctx, types.NamespacedName{Namespace: postgres.Namespace, Name: deploymentName}, &postgresDeployment); err != nil {
+		if client.IgnoreNotFound(err) != nil {
+			log.Error(err, "Failed to get Deployment")
+			return nil, err
+		}
+
+		log.Info("Deployment not found, creating new one")
+	}
+
+	if postgresDeployment.Name != "" {
+		log.Info("Deployment already exists", "deploymentName", postgresDeployment.Name)
+		return &postgresDeployment, nil
+	}
+
+	labels := map[string]string{
+		"app":      "postgres",
+		"instance": postgres.Name,
+	}
+
+	postgresDeployment = appsv1.Deployment{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      deploymentName,
+			Namespace: postgres.Namespace,
+			Labels:    labels,
+		},
+		Spec: appsv1.DeploymentSpec{
+			Replicas: ptr.To(int32(1)),
+			Selector: &metav1.LabelSelector{
+				MatchLabels: labels,
+			},
+			Template: corev1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: labels,
+				},
+				Spec: corev1.PodSpec{
+					Containers: []corev1.Container{
+						{
+							Name:  "postgres",
+							Image: "postgres:15",
+							Env: []corev1.EnvVar{
+								{
+									Name:  "POSTGRES_DB",
+									Value: "postgres",
+								},
+								{
+									Name:  "POSTGRES_USER",
+									Value: "postgres",
+								},
+								{
+									Name:  "POSTGRES_PASSWORD",
+									Value: "password",
+								},
+							},
+							Ports: []corev1.ContainerPort{
+								{
+									ContainerPort: 5432,
+									Name:          "postgres",
+									Protocol:      corev1.ProtocolTCP,
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	// Set owner reference for Deployment
+	if err := controllerutil.SetControllerReference(&postgres, &postgresDeployment, r.Scheme); err != nil {
+		log.Error(err, "Failed to set owner reference for Deployment")
+		return nil, err
+	}
+
+	// Create Deployment
+	if err := r.Client.Create(ctx, &postgresDeployment); err != nil {
+		if client.IgnoreAlreadyExists(err) != nil {
+			log.Error(err, "Failed to create Deployment")
+			return nil, err
+		}
+	}
+
+	return &postgresDeployment, nil
 }
 
 // SetupWithManager sets up the controller with the Manager.
 func (r *PostgresReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&databasev1.Postgres{}).
+		Owns(&appsv1.Deployment{}).
 		Named("postgres").
 		Complete(r)
 }
