372c312e860b58f579c7c8ed02e25fe726b624ec
nivekithan <nivekithan363@gmail.com>
2025-08-12 15:39:15 +0530

    Update postgres versiont o 17

--- controller/internal/controller/postgres_controller.go
+++ controller/internal/controller/postgres_controller.go
@@ -140,7 +140,7 @@ func (r *PostgresReconciler) ensureDeployment(ctx context.Context, postgres data
 					Containers: []corev1.Container{
 						{
 							Name:  "postgres",
-							Image: "postgres:15",
+							Image: "postgres:17",
 							Env: []corev1.EnvVar{
 								{
 									Name:  "POSTGRES_DB",
