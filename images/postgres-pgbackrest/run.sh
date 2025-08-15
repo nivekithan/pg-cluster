a2eb6bc5ef02d95271f7588291bf1d88f305174b
nivekithan <nivekithan363@gmail.com>
2025-08-15 13:03:44 +0530

    Working run.sh

--- images/postgres-pgbackrest/run.sh
+++ images/postgres-pgbackrest/run.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 docker build -t mypgimg . && \
-cid=$(docker run -d --rm --env-file .env -v postgres-data:/var/lib/postgresql/data mypgimg) && \
+cid=$(docker run -d --rm --env-file .env -v postgres-data:/var/lib/postgresql/data mypgimg postgres) && \
 echo "Container ID: $cid" && \
-docker exec -it $cid bash
+if [ -t 0 ]; then
+    docker exec -it $cid bash
+else
+    echo "Non-interactive environment detected. Container is running in background."
+    echo "Use: docker exec -it $cid bash"
+fi
 # Container auto-removes when stopped due to --rm flag
