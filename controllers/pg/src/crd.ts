efdf37d3bf7cf3c2848177b5f062f4da6a551062
Nivekithan <nivekithan363@gmail.com>
2025-08-31 12:24:10 +0530

    move reconcile loop to crd

--- controllers/pg/src/crd.ts
+++ controllers/pg/src/crd.ts
@@ -1,6 +1,13 @@
 import z, { ZodObject } from "zod";
 import { Crd } from "./lib/crd.ts";
 import { logger } from "./logger.ts";
+import { Effect } from "effect";
+import {
+  createNamespacedDeployment,
+  readNamespacedDeployment,
+} from "./lib/deloyment.ts";
+import type { KubeConfig } from "@kubernetes/client-node";
+import type { Logger } from "pino";
 
 const pgCrdSpec = z.object({
   spec: z.object({
@@ -13,4 +20,93 @@ export const pgCrd = new Crd({
   kind: "Postgres",
   spec: pgCrdSpec,
   logger: logger,
+});
+
+export const reconcileCrd = Effect.fn("reconcileCrd")(function* ({
+  name,
+  namespace,
+  pgCrdApi,
+  kc,
+  logger,
+}: {
+  name: string;
+  namespace: string;
+  pgCrdApi: ReturnType<Crd<typeof pgCrdSpec>["getApi"]>;
+  kc: KubeConfig;
+  logger: Logger;
+}) {
+  const childLogger = logger.child({ postgresCrd: { name, namespace } });
+
+  childLogger.info({ action: "RECONCILE_LOOP_STARTED" });
+
+  const postgres = yield* pgCrdApi.getNamespacedObject({
+    name,
+    namespace,
+  });
+
+  childLogger.debug({ msg: "Fetched Postgres object", postgres });
+
+  childLogger.info({
+    action: "CHECKING_EXISTING_DEPLOYMENT",
+    args: { name: "busybox", namespace },
+  });
+
+  const existingDeployment = yield* readNamespacedDeployment({
+    kc,
+    name,
+    namespace,
+  }).pipe(Effect.catchTag("DeploymentNotFound", () => Effect.succeed(null)));
+
+  childLogger.info({
+    action: "CHECKING_EXISTING_DEPLOYMENT_RESULT",
+    args: { name: "busybox", namespace },
+    deployment: existingDeployment,
+  });
+
+  if (!existingDeployment) {
+    childLogger.info({ action: "CREATING_BUSYBOX_DEPLOYMENT" });
+    const busyboxDeployment = yield* createNamespacedDeployment({
+      kc,
+      namespace: namespace,
+      body: {
+        apiVersion: "apps/v1",
+        kind: "Deployment",
+        metadata: {
+          name: "busybox",
+          namespace: namespace,
+        },
+        spec: {
+          replicas: 1,
+          selector: {
+            matchLabels: {
+              app: "busybox",
+            },
+          },
+          template: {
+            metadata: {
+              labels: {
+                app: "busybox",
+              },
+            },
+            spec: {
+              containers: [
+                {
+                  name: "busybox",
+                  image: "busybox",
+                  command: ["sh", "-c", "echo Hello Kubernetes! && sleep 3600"],
+                },
+              ],
+            },
+          },
+        },
+      },
+    });
+
+    childLogger.info({
+      action: "CREATED_BUSYBOX_DEPLOYMENT",
+      deployment: busyboxDeployment,
+    });
+  }
+
+  childLogger.info({ action: "RECONCILE_LOOP_COMPLETED" });
 });
--- controllers/pg/src/index.ts
+++ controllers/pg/src/index.ts
@@ -4,19 +4,14 @@ import {
   KubeConfig,
   makeInformer,
 } from "@kubernetes/client-node";
-import { pgCrd } from "./crd.ts";
+import { pgCrd, reconcileCrd } from "./crd.ts";
 import { logger } from "./logger.ts";
 import { Effect } from "effect";
-import {
-  createNamespacedDeployment,
-  readNamespacedDeployment,
-} from "./lib/deloyment.ts";
 
 const kc = new KubeConfig();
 kc.loadFromDefault();
 
 const pgCrdApi = pgCrd.getApi(kc);
-const appsApi = kc.makeApiClient(AppsV1Api);
 
 const podsInformer = makeInformer(kc, pgCrd.apiPath(), async () => {
   try {
@@ -28,89 +23,6 @@ const podsInformer = makeInformer(kc, pgCrd.apiPath(), async () => {
   }
 });
 
-const reconcileCrd = Effect.fn("reconcileCrd")(function* ({
-  name,
-  namespace,
-}: {
-  name: string;
-  namespace: string;
-}) {
-  const childLogger = logger.child({ postgresCrd: { name, namespace } });
-
-  childLogger.info({ action: "RECONCILE_LOOP_STARTED" });
-
-  const postgres = yield* pgCrdApi.getNamespacedObject({
-    name,
-    namespace,
-  });
-
-  childLogger.debug({ msg: "Fetched Postgres object", postgres });
-
-  childLogger.info({
-    action: "CHECKING_EXISTING_DEPLOYMENT",
-    args: { name: "busybox", namespace },
-  });
-
-  const existingDeployment = yield* readNamespacedDeployment({
-    kc,
-    name,
-    namespace,
-  }).pipe(Effect.catchTag("DeploymentNotFound", () => Effect.succeed(null)));
-
-  childLogger.info({
-    action: "CHECKING_EXISTING_DEPLOYMENT_RESULT",
-    args: { name: "busybox", namespace },
-    deployment: existingDeployment,
-  });
-
-  if (!existingDeployment) {
-    childLogger.info({ action: "CREATING_BUSYBOX_DEPLOYMENT" });
-    const busyboxDeployment = yield* createNamespacedDeployment({
-      kc,
-      namespace: namespace,
-      body: {
-        apiVersion: "apps/v1",
-        kind: "Deployment",
-        metadata: {
-          name: "busybox",
-          namespace: namespace,
-        },
-        spec: {
-          replicas: 1,
-          selector: {
-            matchLabels: {
-              app: "busybox",
-            },
-          },
-          template: {
-            metadata: {
-              labels: {
-                app: "busybox",
-              },
-            },
-            spec: {
-              containers: [
-                {
-                  name: "busybox",
-                  image: "busybox",
-                  command: ["sh", "-c", "echo Hello Kubernetes! && sleep 3600"],
-                },
-              ],
-            },
-          },
-        },
-      },
-    });
-
-    childLogger.info({
-      action: "CREATED_BUSYBOX_DEPLOYMENT",
-      deployment: busyboxDeployment,
-    });
-  }
-
-  childLogger.info({ action: "RECONCILE_LOOP_COMPLETED" });
-});
-
 podsInformer.on("add", (event) => {
   if (!event.metadata?.name || !event.metadata?.namespace) return;
 
@@ -118,6 +30,9 @@ podsInformer.on("add", (event) => {
     reconcileCrd({
       name: event.metadata?.name,
       namespace: event.metadata?.namespace,
+      pgCrdApi,
+      kc,
+      logger,
     }),
   );
 });
@@ -129,6 +44,9 @@ podsInformer.on("update", (event) => {
     reconcileCrd({
       name: event.metadata?.name,
       namespace: event.metadata?.namespace,
+      pgCrdApi,
+      kc,
+      logger,
     }),
   );
 });
@@ -140,6 +58,9 @@ podsInformer.on("delete", (event) => {
     reconcileCrd({
       name: event.metadata?.name,
       namespace: event.metadata?.namespace,
+      pgCrdApi,
+      kc,
+      logger,
     }),
   );
 });
@@ -151,6 +72,9 @@ podsInformer.on("change", (event) => {
     reconcileCrd({
       name: event.metadata?.name,
       namespace: event.metadata?.namespace,
+      pgCrdApi,
+      kc,
+      logger,
     }),
   );
 });
